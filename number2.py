38c458b3a48d68d4d27fffa5c91854584ef74552
Ganesh Rajabhau Salke <126677167+Ganeshsalke@users.noreply.github.com>
2023-12-28 01:37:57 +0530

    add

---
+++ number2.py
@@ -0,0 +1,12 @@
+#it is used for the select letter and data from the table
+name='ganesh'
+
+print(name[4])
+print(name[2:])#2: it is used for select after 2 data.
+print(name[:2])#:2 it is used for select before 2 data
+print(name[2:5])#2:5 it is select data between 2:5 points.
+print(name.upper())#upper it is used for convert upppercase letter
+print(name.lower())#lower it is used for convert all string into lowercase.
+print(name.isupper())#.isupper or islower it is used for the check the given string is uppper or lower.
+print(len(name))#len it is used for the check length of the string
+print(name.replace('g','D'))#replace is mostly used for the replace new string in old string
---
+++ string.py
@@ -0,0 +1,11 @@
+#Strings into the python
+#select number,len,uppercase,lowercase,replace this are the functioms into the python
+name = 'ganesh salke patil'
+print(name[0])
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.islower())
+print(name.isupper())
+print(name.upper().isupper())
+print(name.replace('ganesh','GD'))
