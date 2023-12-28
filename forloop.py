304f7fb576c427a3674b7caa539560bd856e1e3d
Ganesh Rajabhau Salke <126677167+Ganeshsalke@users.noreply.github.com>
2023-12-29 01:18:56 +0530

    add

---
+++ forloop.py
@@ -0,0 +1,30 @@
+#for loop in the python 
+for letter in 'dada':
+    print(letter)
+    #this is a way of for loop
+data=['gana']
+for no in data:
+    print(data)
+    #/''''''''''''''''''''''''''''''''''''''''''''''''#
+    my_list=['cpp','java','python','sql']
+
+    for language in my_list:
+        print(language)
+    #'''''''''''''''''''''''''''''''''''''''''''''''#
+        my_dict={
+            'name':'gana',
+            'age':20,
+            'love':'aai dada',
+            'Qualification':'IT Engineer'
+        }
+        for info in my_dict:
+            print(info)
+           
+    #'''''''''''''''''''''''''''''''''''''''''''''''#
+           
+    age_is=(1,2,4,5,6,7) 
+    for  x in age_is:
+        print(x)
+        print(type(age_is))
+            
+                
---
+++ if...else.py
@@ -0,0 +1,10 @@
+#if and else is use for check condition true or false in that time it is mostly used it is called if---else.
+#able to vote.
+age=int(input('Enter your age here...!'))
+
+if age>18:
+    print('you have right to voting...!')
+elif age<18:
+    print('you have dosent right to ...!')
+else:
+    print('you are not in earth...!')
