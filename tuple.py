dee3391b643e42d5a5ac95aa67fa3ad66ef91c9e
Ganesh Rajabhau Salke <126677167+Ganeshsalke@users.noreply.github.com>
2023-12-30 01:28:25 +0530

    ad

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#datatype tuple
+three_number=(1,3,4,5,7)
+print(three_number)#it used for call tuples .
+print(type(three_number))#call or print tuple
+print(len(three_number))#it used for check length of the tuple
+
+strings=('gana','athu','kunal','himu','dyanu')
+print(strings)
+print(strings[1])
---
+++ user.py
@@ -0,0 +1,7 @@
+#user input in python 
+#input("Enter your name:")
+#this the simple way to get input in pythonn
+name =str(input('Enter your name:'))
+age = int(input('Enter your age: '))
+print(name)
+print(age)
